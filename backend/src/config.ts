efdbaaf53f55a02af3e7cddac90e936536aab589
catcatcat8 <lebedev666e@gmail.com>
2023-04-05 17:25:12 +0300

    zxc

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,4 +3,6 @@ export default {
     ':date[web] :method :url :status :res[content-length] - :response-time ms',
   port: 3000,
   openseaApiBaseUrl: 'https://opensea15.p.rapidapi.com/v2/',
+  seaportV4: '0x00000000000001ad428e4906aE43D8F9852d0dD6',
+  maticRpc: 'https://polygon-rpc.com',
 }
